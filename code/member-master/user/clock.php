05ea118244fe3e21593b636aea8e1c1b54dfe0ff
h <45162233+onlyhyl@users.noreply.github.com>
2019-04-11 10:03:51 +0800

    Add files via upload

---
+++ code/member-master/user/clock.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> </title>
+</head>
+
+<body>
+<div class="clock">
+	<?php
+		$time1 = time();
+		$time2 = strtotime("2019-1-4");
+		$sub = ceil(($time2 - $time1)/3600);
+
+	echo "距离放假还有$sub" . "小时！" . '<br>';
+	?>
+</div>
+</body>
+</html>
